f814b93ae3b3cfbe6c040b8b1e7daa0da8d1fe55
yasmnlc <lc.yasmn@gmail.com>
2025-07-06 09:47:26 -0300

    Add CrudItemApp

--- CrudItemApp/app/build.gradle.kts
+++ CrudItemApp/app/build.gradle.kts
@@ -1,4 +1,3 @@
-gradle
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -17,6 +16,9 @@ android {
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -43,6 +45,13 @@ android {
     }
 
     composeOptions {
+        kotlinCompilerExtensionVersion = "1.6.0"
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
@@ -52,22 +61,24 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore-ktx")
 
     implementation(platform(libs.androidx.compose.bom))
+
     implementation(libs.androidx.ui)
-    implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.tooling.preview)
     debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.androidx.material3)
 
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-
     implementation("androidx.navigation:navigation-compose:2.7.2")
     implementation(libs.androidx.core.ktx)
+
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.test.manifest)
 }
